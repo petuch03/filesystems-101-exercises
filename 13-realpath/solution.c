3dbf9406cc1521d16303067a91d9e7bf33e79874
Egor Safronov <egor.safronov@jetbrains.com>
2024-10-27 14:51:04 +0200

    13 v5

--- 13-realpath/solution.c
+++ 13-realpath/solution.c
@@ -9,101 +9,111 @@
 
 #define MAX_PATH 4096
 
-static void handle_path_component(char* res, size_t* pos, const char* comp) {
+void abspath(const char *path) {
+    char *result = malloc(MAX_PATH);
+    char initial_path[MAX_PATH];
+    char link_buf[MAX_PATH];
+    char segment[MAX_PATH];
     struct stat st;
-    char link_target[MAX_PATH];
-    size_t comp_len = strlen(comp);
-
-    if (strcmp(comp, ".") == 0) {
-        return;
-    }
-
-    if (strcmp(comp, "..") == 0) {
-        if (*pos > 1) {
-            while (*pos > 1 && res[*pos - 1] == '/') (*pos)--;
-            while (*pos > 1 && res[*pos - 1] != '/') (*pos)--;
-        }
-        return;
-    }
-
-    if (res[*pos - 1] != '/') {
-        res[*pos] = '/';
-        (*pos)++;
-    }
-
-    memcpy(res + *pos, comp, comp_len);
-    *pos += comp_len;
-    res[*pos] = '\0';
-
-    if (lstat(res, &st) != 0) {
-        *pos -= comp_len;
-        if (*pos > 0 && res[*pos - 1] == '/')
-            res[--(*pos)] = '\0';
-        res[*pos] = '\0';
-        report_error(res[0] == '\0' ? "/" : res, comp, errno);
-        exit(1);
-    }
-
-    if (S_ISLNK(st.st_mode)) {
-        ssize_t link_len = readlink(res, link_target, sizeof(link_target) - 1);
-        if (link_len < 0) {
-            res[*pos - comp_len - 1] = '\0';
-            report_error(res, comp, errno);
-            exit(1);
-        }
-        link_target[link_len] = '\0';
-
-        if (link_target[0] == '/') {
-            *pos = 1;
-        } else {
-            while (*pos > 1 && res[*pos - 1] != '/') (*pos)--;
-        }
-        res[*pos] = '\0';
-
-        char* save_ptr;
-        char* link_copy = strdup(link_target);
-        char* link_comp = strtok_r(link_copy, "/", &save_ptr);
-
-        while (link_comp) {
-            handle_path_component(res, pos, link_comp);
-            link_comp = strtok_r(NULL, "/", &save_ptr);
-        }
-        free(link_copy);
-    }
-}
-
-void abspath(const char* path) {
-    if (!path || !*path) {
-        report_path("/");
-        return;
-    }
-
-    char res[MAX_PATH];
-    size_t pos = 1;
-    res[0] = '/';
-    res[1] = '\0';
-
-    char* path_copy = strdup(path);
-    char* save_ptr;
-    char* comp;
-
-    if (path[0] == '/') {
-        comp = strtok_r(path_copy + 1, "/", &save_ptr);
+    
+    if (!result) exit(2);
+    result[0] = '\0';
+    
+    if (path[0] != '/') {
+        snprintf(initial_path, sizeof(initial_path), "/%s", path);
     } else {
-        comp = strtok_r(path_copy, "/", &save_ptr);
+        snprintf(initial_path, sizeof(initial_path), "%s", path);
     }
-
-    while (comp) {
-        handle_path_component(res, &pos, comp);
-        comp = strtok_r(NULL, "/", &save_ptr);
+    
+    char *current = initial_path;
+    while (current) {
+        char *next = strchr(current + 1, '/');
+        size_t seg_len;
+        
+        if (next) {
+            seg_len = next - current;
+        } else {
+            seg_len = strlen(current);
+        }
+        
+        memcpy(segment, current, seg_len);
+        segment[seg_len] = '\0';
+        
+        if (strcmp(segment, "/.") == 0) {
+            current = next;
+            continue;
+        }
+        
+        if (strcmp(segment, "/..") == 0) {
+            if (result[0] != '\0') {
+                char *last = strrchr(result, '/');
+                if (last != NULL) {
+                    *last = '\0';
+                }
+            }
+            current = next;
+            continue;
+        }
+        
+        if (result[0] != '\0' && result[strlen(result) - 1] == '/' && segment[0] == '/') {
+            snprintf(result + strlen(result), MAX_PATH - strlen(result), "%s", segment + 1);
+        } else {
+            snprintf(result + strlen(result), MAX_PATH - strlen(result), "%s", segment);
+        }
+        
+        ssize_t link_len = readlink(result, link_buf, MAX_PATH - 1);
+        if (link_len != -1) {
+            link_buf[link_len] = '\0';
+            if (link_buf[0] == '/') {
+                snprintf(result, MAX_PATH, "%s", link_buf);
+            } else {
+                char *tmp_path = malloc(MAX_PATH);
+                if (!tmp_path) exit(2);
+                
+                if (next) {
+                    snprintf(tmp_path, MAX_PATH, "/%s%s", link_buf, next);
+                } else {
+                    snprintf(tmp_path, MAX_PATH, "/%s", link_buf);
+                }
+                
+                snprintf(initial_path, MAX_PATH, "%s", tmp_path);
+                free(tmp_path);
+                result[0] = '\0';
+                current = initial_path;
+                continue;
+            }
+        }
+        
+        if (stat(result, &st) == -1) {
+            char curr_component[MAX_PATH];
+            snprintf(curr_component, MAX_PATH, "%s", segment + 1);
+            
+            if (result[0] != '\0') {
+                if (result[strlen(result) - 1] == '/') {
+                    result[strlen(result) - 1] = '\0';
+                }
+                char *last = strrchr(result, '/');
+                if (last) {
+                    *(last + 1) = '\0';
+                }
+            }
+            
+            if (curr_component[strlen(curr_component) - 1] == '/') {
+                curr_component[strlen(curr_component) - 1] = '\0';
+            }
+            
+            report_error(result[0] == '\0' ? "/" : result, curr_component, errno);
+            free(result);
+            return;
+        }
+        
+        if (S_ISDIR(st.st_mode) && result[strlen(result) - 1] != '/') {
+            snprintf(result + strlen(result), MAX_PATH - strlen(result), "/");
+        }
+        
+        current = next;
     }
-
-    struct stat st;
-    if (pos > 0 && stat(res, &st) == 0 && S_ISDIR(st.st_mode) && res[pos - 1] != '/') {
-        res[pos++] = '/';
-        res[pos] = '\0';
-    }
-
-    free(path_copy);
-    report_path(res);
+    
+    report_path(result);
+    free(result);
 }
