814b809b150ecdba3c6290fb6ecccf1abb6885b0
Egor Safronov <egor.safronov@jetbrains.com>
2024-10-27 16:34:15 +0200

    03 new v2

--- 03-io_uring/solution.c
+++ 03-io_uring/solution.c
@@ -8,7 +8,7 @@
 #define QUEUE_DEPTH 4
 
 struct io_data {
-    void *buf;
+    char *buf;
     size_t size;
     off_t offset;
     int read_done;
@@ -18,38 +18,38 @@ int copy(int in, int out) {
     struct io_uring ring;
     struct io_uring_sqe *sqe;
     struct io_uring_cqe *cqe;
-    struct io_data iodata[QUEUE_DEPTH];
-    int ret = 0, pending_reads = 0, pending_writes = 0;
-    off_t current_offset = 0;
-    int inflight = 0;
+    struct io_data *data[QUEUE_DEPTH] = {0};
+    int ret, i, pending = 0;
 
     ret = io_uring_queue_init(QUEUE_DEPTH * 2, &ring, 0);
     if (ret < 0)
         return -errno;
 
-    for (int i = 0; i < QUEUE_DEPTH; i++) {
-        iodata[i].buf = malloc(COPY_BLOCK_SIZE);
-        if (!iodata[i].buf) {
+    for (i = 0; i < QUEUE_DEPTH; i++) {
+        data[i] = malloc(sizeof(struct io_data));
+        if (!data[i]) {
             ret = -ENOMEM;
             goto cleanup;
         }
-        iodata[i].offset = 0;
-        iodata[i].read_done = 0;
+        data[i]->buf = malloc(COPY_BLOCK_SIZE);
+        if (!data[i]->buf) {
+            ret = -ENOMEM;
+            goto cleanup;
+        }
+        data[i]->offset = i * COPY_BLOCK_SIZE;
+        data[i]->read_done = 0;
     }
 
-    for (int i = 0; i < QUEUE_DEPTH; i++) {
+    // Submit initial read requests
+    for (i = 0; i < QUEUE_DEPTH; i++) {
         sqe = io_uring_get_sqe(&ring);
         if (!sqe) {
             ret = -EAGAIN;
             goto cleanup;
         }
-
-        iodata[i].offset = current_offset;
-        io_uring_prep_read(sqe, in, iodata[i].buf, COPY_BLOCK_SIZE, current_offset);
-        io_uring_sqe_set_data(sqe, &iodata[i]);
-        current_offset += COPY_BLOCK_SIZE;
-        pending_reads++;
-        inflight++;
+        io_uring_prep_read(sqe, in, data[i]->buf, COPY_BLOCK_SIZE, data[i]->offset);
+        io_uring_sqe_set_data(sqe, data[i]);
+        pending++;
     }
 
     ret = io_uring_submit(&ring);
@@ -58,71 +58,57 @@ int copy(int in, int out) {
         goto cleanup;
     }
 
-    while (inflight > 0) {
+    off_t next_offset = QUEUE_DEPTH * COPY_BLOCK_SIZE;
+
+    while (pending > 0) {
         ret = io_uring_wait_cqe(&ring, &cqe);
         if (ret < 0) {
             ret = -errno;
             goto cleanup;
         }
 
-        struct io_data *data = io_uring_cqe_get_data(cqe);
-        int res = cqe->res;
+        struct io_data *current = io_uring_cqe_get_data(cqe);
+        int bytes = cqe->res;
         io_uring_cqe_seen(&ring, cqe);
 
-        if (res < 0) {
-            ret = res;
+        if (bytes < 0) {
+            ret = bytes;
             goto cleanup;
         }
 
-        if (!data->read_done) {
-            data->size = res;
-            data->read_done = 1;
-            pending_reads--;
+        pending--;
 
-            if (res > 0) {
+        if (!current->read_done) {
+            // Read completed
+            current->size = bytes;
+            current->read_done = 1;
+
+            if (bytes > 0) {
+                // Submit write
                 sqe = io_uring_get_sqe(&ring);
                 if (!sqe) {
                     ret = -EAGAIN;
                     goto cleanup;
                 }
+                io_uring_prep_write(sqe, out, current->buf, bytes, current->offset);
+                io_uring_sqe_set_data(sqe, current);
+                pending++;
 
-                io_uring_prep_write(sqe, out, data->buf, data->size, data->offset);
-                io_uring_sqe_set_data(sqe, data);
-                pending_writes++;
-
-                if (res == COPY_BLOCK_SIZE) {
+                if (bytes == COPY_BLOCK_SIZE) {
                     sqe = io_uring_get_sqe(&ring);
                     if (!sqe) {
                         ret = -EAGAIN;
                         goto cleanup;
                     }
-
-                    struct io_data *new_data = NULL;
-                    for (int i = 0; i < QUEUE_DEPTH; i++) {
-                        if (!iodata[i].read_done || iodata[i].size == 0) {
-                            new_data = &iodata[i];
-                            break;
-                        }
-                    }
-
-                    if (new_data) {
-                        new_data->offset = current_offset;
-                        new_data->read_done = 0;
-                        io_uring_prep_read(sqe, in, new_data->buf, COPY_BLOCK_SIZE, current_offset);
-                        io_uring_sqe_set_data(sqe, new_data);
-                        current_offset += COPY_BLOCK_SIZE;
-                        pending_reads++;
-                        inflight++;
-                    }
+                    current->offset = next_offset;
+                    current->read_done = 0;
+                    io_uring_prep_read(sqe, in, current->buf, COPY_BLOCK_SIZE, next_offset);
+                    io_uring_sqe_set_data(sqe, current);
+                    next_offset += COPY_BLOCK_SIZE;
+                    pending++;
                 }
             }
-        } else {
-            pending_writes--;
-            data->size = 0;
-            data->read_done = 0;
         }
-
-        inflight = pending_reads + pending_writes;
 
         ret = io_uring_submit(&ring);
         if (ret < 0) {
@@ -134,8 +120,12 @@ int copy(int in, int out) {
     ret = 0;
 
 cleanup:
-    for (int i = 0; i < QUEUE_DEPTH; i++) {
-        free(iodata[i].buf);
+    for (i = 0; i < QUEUE_DEPTH; i++) {
+        if (data[i]) {
+            if (data[i]->buf)
+                free(data[i]->buf);
+            free(data[i]);
+        }
     }
     io_uring_queue_exit(&ring);
     return ret;
