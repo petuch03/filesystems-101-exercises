7c5f01101c04d05aeac9e4a0f764d972b0cbcabe
Egor Safronov <egor.safronov@jetbrains.com>
2024-10-27 16:29:30 +0200

    03 new v1

--- 03-io_uring/solution.c
+++ 03-io_uring/solution.c
@@ -1,228 +1,142 @@
-#include <stdio.h>
+#include <solution.h>
+#include <liburing.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <sys/stat.h>
-#include <liburing.h>
 
-#define BUFFER_SZ (256 * 1024)
+#define BLOCK_SIZE (256 * 1024)  // 256KB blocks
 #define QUEUE_DEPTH 4
 
-struct xfer_ctx {
-    char *buf;
-    size_t len;
-    off_t pos;
-    int is_read;
-} __attribute__((aligned(8)));
+struct io_data {
+    void *buf;
+    size_t size;
+    off_t offset;
+    int read_done;
+};
 
-static inline int setup_ring(struct io_uring *ring) {
-    //DBG_LOG("init ring with qd=%d", QUEUE_DEPTH);
-    return io_uring_queue_init(QUEUE_DEPTH, ring, 0);
-}
-
-static off_t get_input_size(int fd) {
-    struct stat st;
-    return fstat(fd, &st) == 0 ? st.st_size : -1;
-}
-
-static int push_read(struct io_uring *ring, int fd, struct xfer_ctx *ctx, size_t bytes) {
-    struct io_uring_sqe *sqe;
-    
-    //DBG_LOG("push read: pos=%lu sz=%lu", ctx->pos, bytes);
-    
-    if (!(sqe = io_uring_get_sqe(ring)))
-        return -1;
-        
-    ctx->buf = malloc(bytes);
-    if (!ctx->buf)
-        return -ENOMEM;
-    
-    ctx->len = bytes;
-    ctx->is_read = 1;
-    
-    io_uring_prep_read(sqe, fd, ctx->buf, bytes, ctx->pos);
-    io_uring_sqe_set_data(sqe, ctx);
-    
-    return 0;
-}
-
-static int push_write(struct io_uring *ring, int fd, struct xfer_ctx *ctx) {
-    struct io_uring_sqe *sqe;
-    
-    //DBG_LOG("push write: pos=%lu sz=%lu", ctx->pos, ctx->len);
-    
-    if (!(sqe = io_uring_get_sqe(ring)))
-        return -1;
-    
-    ctx->is_read = 0;
-    
-    io_uring_prep_write(sqe, fd, ctx->buf, ctx->len, ctx->pos);
-    io_uring_sqe_set_data(sqe, ctx);
-    
-    return io_uring_submit(ring);
-}
-
-int copy(int in_fd, int out_fd) {
+int copy(int in, int out) {
     struct io_uring ring;
+    struct io_uring_sqe *sqe;
     struct io_uring_cqe *cqe;
-    off_t total_sz, remaining;
-    size_t curr_off = 0;
-    int active_xfers = 0;
-    int pending_writes = 0;
-    int ret = 0;
-    
-    //DBG_LOG("starting copy operation");
-    
-    total_sz = get_input_size(in_fd);
-    if (total_sz < 0)
+    struct io_data iodata[QUEUE_DEPTH];
+    int ret = 0, pending_reads = 0, pending_writes = 0;
+    off_t current_offset = 0;
+    int inflight = 0;
+
+    ret = io_uring_queue_init(QUEUE_DEPTH * 2, &ring, 0);
+    if (ret < 0)
         return -errno;
-    
-    if (setup_ring(&ring) < 0)
-        return -errno;
-        
-    remaining = total_sz;
-    
-    // Initial read batch
-    for (int i = 0; i < QUEUE_DEPTH && remaining > 0; i++) {
-        struct xfer_ctx *ctx = malloc(sizeof(*ctx));
-        if (!ctx) {
+
+    for (int i = 0; i < QUEUE_DEPTH; i++) {
+        iodata[i].buf = malloc(BLOCK_SIZE);
+        if (!iodata[i].buf) {
             ret = -ENOMEM;
             goto cleanup;
         }
-        
-        size_t chunk = remaining > BUFFER_SZ ? BUFFER_SZ : remaining;
-        ctx->pos = curr_off;
-        
-        if (push_read(&ring, in_fd, ctx, chunk) < 0) {
-            free(ctx);
+        iodata[i].offset = 0;
+        iodata[i].read_done = 0;
+    }
+
+    for (int i = 0; i < QUEUE_DEPTH; i++) {
+        sqe = io_uring_get_sqe(&ring);
+        if (!sqe) {
+            ret = -EAGAIN;
+            goto cleanup;
+        }
+
+        iodata[i].offset = current_offset;
+        io_uring_prep_read(sqe, in, iodata[i].buf, BLOCK_SIZE, current_offset);
+        io_uring_sqe_set_data(sqe, &iodata[i]);
+        current_offset += BLOCK_SIZE;
+        pending_reads++;
+        inflight++;
+    }
+
+    ret = io_uring_submit(&ring);
+    if (ret < 0) {
+        ret = -errno;
+        goto cleanup;
+    }
+
+    while (inflight > 0) {
+        ret = io_uring_wait_cqe(&ring, &cqe);
+        if (ret < 0) {
             ret = -errno;
             goto cleanup;
         }
-        
-        curr_off += chunk;
-        remaining -= chunk;
-        active_xfers++;
-    }
-    
-    //DBG_LOG("queued initial %d reads", active_xfers);
-    
-    if ((ret = io_uring_submit(&ring)) < 0)
-        goto cleanup;
-    pending_writes = active_xfers;
-    while (active_xfers > 0) {
-        ret = io_uring_wait_cqe(&ring, &cqe);
-        if (ret < 0)
-            struct io_uring_cqe *cleanup_cqe;
-            while (active_ios > 0) {
-                if (io_uring_peek_cqe(&ring, &cleanup_cqe) == 0) {
-                    struct xfer_ctx *cleanup_ctx = io_uring_cqe_get_data(cleanup_cqe);
-                    if (cleanup_ctx) {
-                        free(cleanup_ctx->buf);
-                        free(cleanup_ctx);
-                    }
-                    io_uring_cqe_seen(&ring, cleanup_cqe);
-                    active_ios--;
-                }
-            }
-            break;
-        struct xfer_ctx *ctx = io_uring_cqe_get_data(cqe);
-        int bytes = cqe->res;
+
+        struct io_data *data = io_uring_cqe_get_data(cqe);
+        int res = cqe->res;
         io_uring_cqe_seen(&ring, cqe);
 
-        if (bytes < 0) {
-            ret = bytes;
-            free(ctx->buf);
-            free(ctx);
-            struct io_uring_cqe *cleanup_cqe;
-            while (active_ios > 0) {
-                if (io_uring_peek_cqe(&ring, &cleanup_cqe) == 0) {
-                    struct xfer_ctx *cleanup_ctx = io_uring_cqe_get_data(cleanup_cqe);
-                    if (cleanup_ctx) {
-                        free(cleanup_ctx->buf);
-                        free(cleanup_ctx);
-                    }
-                    io_uring_cqe_seen(&ring, cleanup_cqe);
-                    active_ios--;
-                }
-            }
-            break;
+        if (res < 0) {
+            ret = res;
+            goto cleanup;
         }
-        if (ctx->is_read) {
-            pending_writes--;
-            ctx->len = bytes;  // actual bytes read
-            
-            if (push_write(&ring, out_fd, ctx) < 0) {
-                ret = -errno;
-                free(ctx->buf);
-                free(ctx);
-                struct io_uring_cqe *cleanup_cqe;
-                while (active_ios > 0) {
-                    if (io_uring_peek_cqe(&ring, &cleanup_cqe) == 0) {
-                        struct xfer_ctx *cleanup_ctx = io_uring_cqe_get_data(cleanup_cqe);
-                        if (cleanup_ctx) {
-                            free(cleanup_ctx->buf);
-                            free(cleanup_ctx);
-                        }
-                        io_uring_cqe_seen(&ring, cleanup_cqe);
-                        active_ios--;
-                    }
-                }
-                break;
-            }
-            
-            if (remaining > 0) {
-                struct xfer_ctx *new_ctx = malloc(sizeof(*new_ctx));
-                if (!new_ctx) {
-                    ret = -ENOMEM;
-                    struct io_uring_cqe *cleanup_cqe;
-                    while (active_ios > 0) {
-                        if (io_uring_peek_cqe(&ring, &cleanup_cqe) == 0) {
-                            struct xfer_ctx *cleanup_ctx = io_uring_cqe_get_data(cleanup_cqe);
-                            if (cleanup_ctx) {
-                                free(cleanup_ctx->buf);
-                                free(cleanup_ctx);
-                            }
-                            io_uring_cqe_seen(&ring, cleanup_cqe);
-                            active_ios--;
-                        }
-                    }
-                    break;
-                }
-                
-                size_t chunk = remaining > BUFFER_SZ ? BUFFER_SZ : remaining;
-                new_ctx->pos = curr_off;
-                
-                if (push_read(&ring, in_fd, new_ctx, chunk) < 0) {
-                    free(new_ctx);
-                    ret = -errno;
-                    struct io_uring_cqe *cleanup_cqe;
-                    while (active_ios > 0) {
-                        if (io_uring_peek_cqe(&ring, &cleanup_cqe) == 0) {
-                            struct xfer_ctx *cleanup_ctx = io_uring_cqe_get_data(cleanup_cqe);
-                            if (cleanup_ctx) {
-                                free(cleanup_ctx->buf);
-                                free(cleanup_ctx);
-                            }
-                            io_uring_cqe_seen(&ring, cleanup_cqe);
-                            active_ios--;
-                        }
-                    }
-                    break;
+
+        if (!data->read_done) {
+            data->size = res;
+            data->read_done = 1;
+            pending_reads--;
+
+            if (res > 0) {
+                sqe = io_uring_get_sqe(&ring);
+                if (!sqe) {
+                    ret = -EAGAIN;
+                    goto cleanup;
                 }
 
-                curr_off += chunk;
-                remaining -= chunk;
+                io_uring_prep_write(sqe, out, data->buf, data->size, data->offset);
+                io_uring_sqe_set_data(sqe, data);
                 pending_writes++;
+
+                if (res == BLOCK_SIZE) {
+                    sqe = io_uring_get_sqe(&ring);
+                    if (!sqe) {
+                        ret = -EAGAIN;
+                        goto cleanup;
+                    }
+
+                    struct io_data *new_data = NULL;
+                    for (int i = 0; i < QUEUE_DEPTH; i++) {
+                        if (!iodata[i].read_done || iodata[i].size == 0) {
+                            new_data = &iodata[i];
+                            break;
+                        }
+                    }
+
+                    if (new_data) {
+                        new_data->offset = current_offset;
+                        new_data->read_done = 0;
+                        io_uring_prep_read(sqe, in, new_data->buf, BLOCK_SIZE, current_offset);
+                        io_uring_sqe_set_data(sqe, new_data);
+                        current_offset += BLOCK_SIZE;
+                        pending_reads++;
+                        inflight++;
+                    }
+                }
             }
         } else {
-            active_xfers--;
-            free(ctx->buf);
-            free(ctx);
+            pending_writes--;
+            data->size = 0;
+            data->read_done = 0;
+        }
+
+        inflight = pending_reads + pending_writes;
+
+        ret = io_uring_submit(&ring);
+        if (ret < 0) {
+            ret = -errno;
+            goto cleanup;
         }
     }
 
+    ret = 0;
+
 cleanup:
-    //DBG_LOG("cleanup: ret=%d", ret);
+    for (int i = 0; i < QUEUE_DEPTH; i++) {
+        free(iodata[i].buf);
+    }
     io_uring_queue_exit(&ring);
     return ret;
 }
