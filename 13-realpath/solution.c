0e455f596fdcbc083c23d375b73fce6084af3db0
Egor Safronov <egor.safronov@jetbrains.com>
2024-10-27 14:45:05 +0200

    13 v2

--- 13-realpath/solution.c
+++ 13-realpath/solution.c
@@ -9,105 +9,98 @@
 
 #define MAX_PATH 4096
 
-static void process_component(char* result, const char* component,
-                            int* result_len, const char* curr_path) {
+static void handle_path_component(char* res, size_t* pos, const char* comp) {
     struct stat st;
-    char link_buf[MAX_PATH];
-    ssize_t link_len;
+    char link_target[MAX_PATH];
+    size_t comp_len = strlen(comp);
 
-    if (strcmp(component, ".") == 0) {
+    if (strcmp(comp, ".") == 0) {
         return;
     }
 
-    if (strcmp(component, "..") == 0) {
-        char *last_slash = strrchr(result, '/');
-        if (last_slash && last_slash != result) {
-            *last_slash = '\0';
-            *result_len = last_slash - result;
+    if (strcmp(comp, "..") == 0) {
+        if (*pos > 1) {
+            while (*pos > 1 && res[*pos - 1] == '/') (*pos)--;
+            while (*pos > 1 && res[*pos - 1] != '/') (*pos)--;
         }
         return;
     }
 
-    if (result[*result_len - 1] != '/') {
-        result[*result_len] = '/';
-        (*result_len)++;
+    if (res[*pos - 1] != '/') {
+        res[*pos] = '/';
+        (*pos)++;
     }
-    strcpy(result + *result_len, component);
-    *result_len += strlen(component);
 
-    if (lstat(result, &st) != 0) {
-        char *last_slash = strrchr(result, '/');
-        *last_slash = '\0';
-        report_error(result, component, errno);
+    memcpy(res + *pos, comp, comp_len);
+    *pos += comp_len;
+    res[*pos] = '\0';
+
+    if (lstat(res, &st) != 0) {
+        res[*pos - comp_len - 1] = '\0';
+        report_error(res, comp, errno);
         exit(1);
     }
 
     if (S_ISLNK(st.st_mode)) {
-        link_len = readlink(result, link_buf, sizeof(link_buf) - 1);
-        if (link_len == -1) {
-            char *last_slash = strrchr(result, '/');
-            *last_slash = '\0';
-            report_error(result, component, errno);
+        ssize_t link_len = readlink(res, link_target, sizeof(link_target) - 1);
+        if (link_len < 0) {
+            res[*pos - comp_len - 1] = '\0';
+            report_error(res, comp, errno);
             exit(1);
         }
-        link_buf[link_len] = '\0';
+        link_target[link_len] = '\0';
 
-        if (link_buf[0] == '/') {
-            *result_len = 0;
-            strcpy(result, "/");
-            *result_len = 1;
+        if (link_target[0] == '/') {
+            *pos = 1;
         } else {
-            char *last_slash = strrchr(result, '/');
-            if (last_slash) {
-                *last_slash = '\0';
-                *result_len = last_slash - result;
-            }
+            while (*pos > 1 && res[*pos - 1] != '/') (*pos)--;
         }
+        res[*pos] = '\0';
 
-        char *link_copy = strdup(link_buf);
-        char *comp = strtok(link_copy, "/");
-        while (comp) {
-            process_component(result, comp, result_len, curr_path);
-            comp = strtok(NULL, "/");
+        char* save_ptr;
+        char* link_copy = strdup(link_target);
+        char* link_comp = strtok_r(link_copy, "/", &save_ptr);
+
+        while (link_comp) {
+            handle_path_component(res, pos, link_comp);
+            link_comp = strtok_r(NULL, "/", &save_ptr);
         }
         free(link_copy);
     }
 }
 
 void abspath(const char* path) {
-    char result[MAX_PATH] = "/";
-    int result_len = 1;
-    char* path_copy;
-    char* component;
-    struct stat st;
-
     if (!path || !*path) {
         report_path("/");
         return;
     }
 
-    path_copy = strdup(path);
+    char res[MAX_PATH];
+    size_t pos = 1;
+    res[0] = '/';
+    res[1] = '\0';
+
+    char* path_copy = strdup(path);
+    char* save_ptr;
+    char* comp;
+
     if (path[0] == '/') {
-        component = strtok(path_copy + 1, "/");
+        comp = strtok_r(path_copy + 1, "/", &save_ptr);
     } else {
-        component = strtok(path_copy, "/");
+        comp = strtok_r(path_copy, "/", &save_ptr);
     }
 
-    while (component) {
-        process_component(result, component, &result_len, path);
-        component = strtok(NULL, "/");
+    while (comp) {
+        handle_path_component(res, &pos, comp);
+        comp = strtok_r(NULL, "/", &save_ptr);
     }
 
-    if (result_len > 0) {
-        if (stat(result, &st) == 0 && S_ISDIR(st.st_mode)) {
-            if (result[result_len - 1] != '/') {
-                result[result_len] = '/';
-                result_len++;
-            }
-        }
+    struct stat st;
+    if (pos > 0 && stat(res, &st) == 0 && S_ISDIR(st.st_mode) && res[pos - 1] != '/') {
+        res[pos++] = '/';
+        res[pos] = '\0';
     }
 
-    result[result_len] = '\0';
     free(path_copy);
-    report_path(result);
+    report_path(res);
 }
