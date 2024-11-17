d87e6fbf246a1d58717461cccfd68ebf5abe98c6
Egor Safronov <egor.safronov@jetbrains.com>
2024-11-17 18:18:21 +0200

    fixup! 04

--- 04-ext2-read-file/solution.c
+++ 04-ext2-read-file/solution.c
@@ -6,8 +6,6 @@
 #include <string.h>
 
 #define EXT2_SUPER_MAGIC 0xEF53
-#define BLOCK_SIZE 1024
-#define INODE_SIZE 128
 
 struct ext2_super_block {
     uint32_t s_inodes_count;
@@ -58,148 +56,125 @@ struct ext2_inode {
 
 static int read_block(int fd, void *buffer, uint32_t block_nr, uint32_t block_size) {
     ssize_t bytes_read = pread(fd, buffer, block_size, block_nr * block_size);
-    if (bytes_read < 0) {
+    if (bytes_read < 0)
         return -errno;
-    }
-    if (bytes_read != (ssize_t)block_size) {
+    if (bytes_read != (ssize_t)block_size)
         return -EIO;
-    }
     return 0;
 }
 
-static int write_data(int fd, const void *buffer, size_t size) {
-    const char *buf = buffer;
-    size_t total_written = 0;
-
-    while (total_written < size) {
-        ssize_t written = write(fd, buf + total_written, size - total_written);
-        if (written < 0) {
-            return -errno;
-        }
-        total_written += written;
-    }
-    return 0;
-}
-
-int dump_file(int img, int inode_nr, int out)
-{
+int dump_file(int img, int inode_nr, int out) {
     struct ext2_super_block sb;
     ssize_t read_size = pread(img, &sb, sizeof(sb), 1024);
-    if (read_size < 0) {
+    if (read_size < 0)
         return -errno;
-    }
-    if (read_size != sizeof(sb)) {
+    if (read_size != sizeof(sb) || sb.s_magic != EXT2_SUPER_MAGIC)
         return -EIO;
-    }
-    if (sb.s_magic != EXT2_SUPER_MAGIC) {
-        return -EINVAL;
-    }
 
     uint32_t block_size = 1024 << sb.s_log_block_size;
 
-    uint32_t block_group = (inode_nr - 1) / sb.s_inodes_per_group;
-    uint32_t inode_index = (inode_nr - 1) % sb.s_inodes_per_group;
-    uint32_t block_group_offset = 2048 + (block_group * sizeof(struct ext2_inode) * sb.s_inodes_per_group);
-    uint32_t inode_offset = block_group_offset + (inode_index * sizeof(struct ext2_inode));
+    uint32_t inodes_per_group = sb.s_inodes_per_group;
+    uint32_t block_group = (inode_nr - 1) / inodes_per_group;
+    uint32_t inode_index = (inode_nr - 1) % inodes_per_group;
+    uint32_t inode_table_offset = 2048 + (block_group * sizeof(struct ext2_inode) * inodes_per_group);
+    uint32_t inode_offset = inode_table_offset + (inode_index * sizeof(struct ext2_inode));
 
     struct ext2_inode inode;
     read_size = pread(img, &inode, sizeof(inode), inode_offset);
-    if (read_size < 0) {
+    if (read_size < 0)
         return -errno;
-    }
-    if (read_size != sizeof(inode)) {
+    if (read_size != sizeof(inode))
         return -EIO;
-    }
 
+    uint32_t remaining_size = inode.i_size;
     char *block_buffer = fs_xmalloc(block_size);
-    int result = 0;
 
-    for (int i = 0; i < 12 && inode.i_block[i] && inode.i_size > 0; i++) {
-        result = read_block(img, block_buffer, inode.i_block[i], block_size);
+    for (int i = 0; i < 12 && remaining_size > 0 && inode.i_block[i]; i++) {
+        int result = read_block(img, block_buffer, inode.i_block[i], block_size);
         if (result < 0) {
             fs_xfree(block_buffer);
             return result;
         }
 
-        uint32_t to_write = (inode.i_size < block_size) ? inode.i_size : block_size;
-        result = write_data(out, block_buffer, to_write);
-        if (result < 0) {
+        uint32_t write_size = (remaining_size < block_size) ? remaining_size : block_size;
+        ssize_t written = write(out, block_buffer, write_size);
+        if (written < 0) {
             fs_xfree(block_buffer);
-            return result;
+            return -errno;
         }
-        inode.i_size -= to_write;
+        remaining_size -= write_size;
     }
 
-    if (inode.i_block[12] && inode.i_size > 0) {
-        uint32_t *indirect_block = fs_xmalloc(block_size);
-        result = read_block(img, indirect_block, inode.i_block[12], block_size);
+    if (inode.i_block[12] && remaining_size > 0) {
+        uint32_t *indirect = fs_xmalloc(block_size);
+        int result = read_block(img, indirect, inode.i_block[12], block_size);
         if (result < 0) {
-            fs_xfree(indirect_block);
+            fs_xfree(indirect);
             fs_xfree(block_buffer);
             return result;
         }
 
-        for (uint32_t i = 0; i < block_size/4 && indirect_block[i] && inode.i_size > 0; i++) {
-            result = read_block(img, block_buffer, indirect_block[i], block_size);
+        for (uint32_t i = 0; i < block_size/4 && remaining_size > 0 && indirect[i]; i++) {
+            result = read_block(img, block_buffer, indirect[i], block_size);
             if (result < 0) {
-                fs_xfree(indirect_block);
+                fs_xfree(indirect);
                 fs_xfree(block_buffer);
                 return result;
             }
 
-            uint32_t to_write = (inode.i_size < block_size) ? inode.i_size : block_size;
-            result = write_data(out, block_buffer, to_write);
-            if (result < 0) {
-                fs_xfree(indirect_block);
+            uint32_t write_size = (remaining_size < block_size) ? remaining_size : block_size;
+            ssize_t written = write(out, block_buffer, write_size);
+            if (written < 0) {
+                fs_xfree(indirect);
                 fs_xfree(block_buffer);
-                return result;
+                return -errno;
             }
-            inode.i_size -= to_write;
+            remaining_size -= write_size;
         }
-        fs_xfree(indirect_block);
+        fs_xfree(indirect);
     }
 
-    if (inode.i_block[13] && inode.i_size > 0) {
-        uint32_t *double_indirect = fs_xmalloc(block_size);
-        result = read_block(img, double_indirect, inode.i_block[13], block_size);
+    if (inode.i_block[13] && remaining_size > 0) {
+        uint32_t *dbl_indirect = fs_xmalloc(block_size);
+        int result = read_block(img, dbl_indirect, inode.i_block[13], block_size);
         if (result < 0) {
-            fs_xfree(double_indirect);
+            fs_xfree(dbl_indirect);
             fs_xfree(block_buffer);
             return result;
         }
 
-        for (uint32_t i = 0; i < block_size/4 && double_indirect[i] && inode.i_size > 0; i++) {
-            uint32_t *indirect_block = fs_xmalloc(block_size);
-            result = read_block(img, indirect_block, double_indirect[i], block_size);
+        for (uint32_t i = 0; i < block_size/4 && remaining_size > 0 && dbl_indirect[i]; i++) {
+            uint32_t *indirect = fs_xmalloc(block_size);
+            result = read_block(img, indirect, dbl_indirect[i], block_size);
             if (result < 0) {
-                fs_xfree(indirect_block);
-                fs_xfree(double_indirect);
+                fs_xfree(indirect);
+                fs_xfree(dbl_indirect);
                 fs_xfree(block_buffer);
                 return result;
             }
 
-            for (uint32_t j = 0; j < block_size/4 && indirect_block[j] && inode.i_size > 0; j++) {
-                result = read_block(img, block_buffer, indirect_block[j], block_size);
+            for (uint32_t j = 0; j < block_size/4 && remaining_size > 0 && indirect[j]; j++) {
+                result = read_block(img, block_buffer, indirect[j], block_size);
                 if (result < 0) {
-                    fs_xfree(indirect_block);
-                    fs_xfree(double_indirect);
+                    fs_xfree(indirect);
+                    fs_xfree(dbl_indirect);
                     fs_xfree(block_buffer);
                     return result;
                 }
 
-                uint32_t to_write = (inode.i_size < block_size) ? inode.i_size : block_size;
-                result = write_data(out, block_buffer, to_write);
-                if (result < 0) {
-                    fs_xfree(indirect_block);
-                    fs_xfree(double_indirect);
+                uint32_t write_size = (remaining_size < block_size) ? remaining_size : block_size;
+                ssize_t written = write(out, block_buffer, write_size);
+                if (written < 0) {
+                    fs_xfree(indirect);
+                    fs_xfree(dbl_indirect);
                     fs_xfree(block_buffer);
-                    return result;
+                    return -errno;
                 }
-                inode.i_size -= to_write;
+                remaining_size -= write_size;
             }
-            fs_xfree(indirect_block);
+            fs_xfree(indirect);
         }
-        fs_xfree(double_indirect);
+        fs_xfree(dbl_indirect);
     }
 
     fs_xfree(block_buffer);
