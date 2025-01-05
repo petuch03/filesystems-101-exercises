fc7604b137f8cd3bedf40d71ee2626e3a3bd84ad
Egor Safronov <egor.safronov@jetbrains.com>
2025-01-05 11:02:45 +0200

    12

--- 12-grpc-prometheus/pkg/parhash/server.go
+++ 12-grpc-prometheus/pkg/parhash/server.go
@@ -55,28 +55,160 @@ type Server struct {
 	conf Config
 
 	sem *semaphore.Weighted
+
+    stop     context.CancelFunc
+    listener net.Listener
+    wg       sync.WaitGroup
+
+    backendClients []hashpb.HashSvcClient
+    nextBackend   atomic.Uint64
+
+    // Prometheus metrics
+    requestCounter      prometheus.Counter
+    subqueryHistograms map[string]prometheus.Observer
 }
 
 func New(conf Config) *Server {
-	return &Server{
+	s := &Server{
 		conf: conf,
 		sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
 	}
+
+	// Initialize Prometheus metrics
+	s.requestCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "parhash",
+		Name:      "nr_requests",
+		Help:      "Number of ParallelHash requests received",
+	})
+
+	// Create histogram for each backend
+	histogramOpts := prometheus.HistogramOpts{
+		Namespace: "parhash",
+		Name:      "subquery_durations",
+		Help:      "Duration of backend hash requests",
+		// 24 buckets from 0.1ms to 10s
+		Buckets: prometheus.ExponentialBuckets(0.0001, 1.7, 24),
+	}
+
+	s.subqueryHistograms = make(map[string]prometheus.Observer, len(conf.BackendAddrs))
+	for _, addr := range conf.BackendAddrs {
+		histogram := prometheus.NewHistogram(prometheus.HistogramOpts{
+			Namespace: histogramOpts.Namespace,
+			Name:      histogramOpts.Name,
+			Help:      histogramOpts.Help,
+			Buckets:   histogramOpts.Buckets,
+			ConstLabels: prometheus.Labels{
+				"backend": addr,
+			},
+		})
+		s.subqueryHistograms[addr] = histogram
+		conf.Prom.MustRegister(histogram)
+	}
+
+	conf.Prom.MustRegister(s.requestCounter)
+	return s
 }
 
 func (s *Server) Start(ctx context.Context) (err error) {
 	defer func() { err = errors.Wrap(err, "Start()") }()
 
-	/* implement me */
+	ctx, s.stop = context.WithCancel(ctx)
+
+	s.backendClients = make([]hashpb.HashSvcClient, len(s.conf.BackendAddrs))
+    	for i, addr := range s.conf.BackendAddrs {
+    		conn, err := grpc.Dial(addr, grpc.WithInsecure())
+    		if err != nil {
+    			return errors.Wrapf(err, "failed to connect to backend %s", addr)
+    		}
+    		s.backendClients[i] = hashpb.NewHashSvcClient(conn)
+    	}
+
+	s.listener, err = net.Listen("tcp", s.conf.ListenAddr)
+    if err != nil {
+        return errors.Wrap(err, "failed to start listener")
+    }
+
+	srv := grpc.NewServer()
+	parhashpb.RegisterParallelHashSvcServer(srv, s)
+
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+		if err := srv.Serve(s.listener); err != nil {
+			_ = err
+		}
+	}()
+
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+		<-ctx.Done()
+		srv.GracefulStop()
+		s.listener.Close()
+	}()
 
 	return nil
 }
 
 func (s *Server) ListenAddr() string {
-	/* implement me */
-	return ""
+	if s.listener == nil {
+		return ""
+	}
+	return s.listener.Addr().String()
 }
 
 func (s *Server) Stop() {
-	/* implement me */
+	if s.stop != nil {
+		s.stop()
+	}
+	s.wg.Wait()
+}
+
+func (s *Server) getNextBackend() (hashpb.HashSvcClient, string) {
+	nextIndex := s.nextBackend.Add(1) % uint64(len(s.backendClients))
+	return s.backendClients[nextIndex], s.conf.BackendAddrs[nextIndex]
+}
+
+func (s *Server) ParallelHash(ctx context.Context, req *parhashpb.ParHashReq) (*parhashpb.ParHashResp, error) {
+	s.requestCounter.Inc()
+
+    if len(req.Data) == 0 {
+		return &parhashpb.ParHashResp{}, nil
+	}
+
+	hashes := make([][]byte, len(req.Data))
+
+	wg := workgroup.New(workgroup.Config{Sem: s.sem})
+
+	for i, data := range req.Data {
+		i, data := i, data
+
+		wg.Go(ctx, func(ctx context.Context) error {
+			backend, backendAddr := s.getNextBackend()
+
+			start := time.Now()
+			defer func() {
+				duration := time.Since(start).Seconds()
+				if histogram := s.subqueryHistograms[backendAddr]; histogram != nil {
+					histogram.Observe(duration)
+				}
+			}()
+
+			resp, err := backend.Hash(ctx, &hashpb.HashReq{Data: data})
+			if err != nil {
+				return errors.Wrapf(err, "backend hash failed for buffer %d", i)
+			}
+
+			hashes[i] = resp.Hash
+			return nil
+		})
+	}
+
+	if err := wg.Wait(); err != nil {
+		return nil, errors.Wrap(err, "parallel hash failed")
+	}
+
+	return &parhashpb.ParHashResp{
+		Hashes: hashes,
+	}, nil
 }
